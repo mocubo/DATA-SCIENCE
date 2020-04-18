533f18b9f7f88adf89943e3be37b031fa99d4dbf
Mónica <63602389+mocubo@users.noreply.github.com>
2020-04-18 09:55:13 +0200

    cambio en el nombre del tercer argumento para que coincida con el de la tarea

--- HOSPITAL DATA/rankhospital.R
+++ HOSPITAL DATA/rankhospital.R
@@ -1,8 +1,8 @@
-rankhospital <- function (state= 'AL',outname= 'heart attack', pos){
+rankhospital <- function (state= 'AL',outname= 'heart attack', num){
                 outcome<- read.csv("outcome-of-care-measures.csv",stringsAsFactors = FALSE )
                 columna <- switch (outname, 'heart attack'=11,
                                    'heart failure'=17,  'pneumonia' =23)
-                if(pos =='best') pos=1
+                if(num =='best') num=1
                 outcome [,columna] <- as.numeric(outcome [,columna])
                 ordenada <- outcome [order(outcome[,columna], outcome$Hospital.Name),]
                 if (is.element (state,ordenada$State)){
@@ -10,8 +10,8 @@ rankhospital <- function (state= 'AL',outname= 'heart attack', pos){
                         filterna <- !is.na(listaestado[,columna])
                         resultado<- listaestado [filterna,]
                         ultima <- nrow(resultado)
-                        if(pos =='worst') pos=ultima
-                        hospital <- listaestado[pos,2]
+                        if(num =='worst') num=ultima
+                        hospital <- listaestado[num,2]
                 }
                 else 
                         hospital <-'estado no disponible'
